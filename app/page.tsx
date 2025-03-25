9fd9f9791075a8adbd22454edb1254840affd667
Alexander <yoda4115@gmail.com>
2025-03-25 14:35:44 -0700

    Start of Front End

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,34 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <>
+      <section className="relative">
+        <nav className="grid md:grid-cols-6 sm:grid-cols-5 w-full  left-0 bg-slate-600 max-w-screen items-center justify-between rounded-b-3xl mt-0 p-4">
+          <h1 className="col-start-1 lg:col-span-2 text-white text-9xl">- Unova National Pokedex</h1>
+            
+            <div className="lg:col-start-3 sm:col-start-2 col-span-2">
+                <input type="text" id="searchText" placeholder="Search" className="px-5 w-full text-gray-900 border-4 border-gray-300 rounded-3xl bg-gray-50 text-base focus:ring-blue-300 focus:border-blue-300"/>
+                <div className="grid grid-cols-3" id="favList"></div>
+            </div>
+            
+            <div className="sm:col-span-2 lg:col-span-1">
+                <button type="button" id="searchBtn" className="w-full mx-3 bg-blue-300 border-blue-400 hover:bg-blue-500 hover:border-blue-600 focus:ring-4 border-4 focus:ring-blue-300 rounded-3xl">Search</button>                    
+            </div>
+            <div className="grid grid-cols-2 sm:col-span-3 sm:col-start-6">
+                <div className="justify-self-center">
+                    <button type="button" id="favBtn" className="bg-blue-300 border-blue-400 hover:bg-blue-500 hover:border-blue-600 border-4 focus:ring-blue-300 rounded-3xl">
+                        <img src="/img/StarOff.png" alt="Favorites Button" className="m-3 object-fill"/>
+                    </button>                    
+                </div>
+                <div className="sm:justify-self-center lg:justify-self-start">
+                    <button type="button" id="shuffleBtn" className="bg-blue-300 border-blue-400 hover:bg-blue-500 hover:border-blue-600 border-4 focus:ring-blue-300 rounded-3xl">
+                        <img src="/img/Shuffle.png" alt="Shuffle" className="m-3 object-fill"/>
+                    </button>                    
+                </div>
+            </div>
+          </nav>
+      </section>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
-}
+    </>
+  )};
+
+    
